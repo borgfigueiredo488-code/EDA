d4db9bd7162ec5a1cc6aaed0100c5edd95b6ccf6
EdilBorg <borgesedil488@gmali.com>
2026-03-26 09:54:57 +0200

    Pastas Organizadas

--- Estrutura-Dados-Trabalho/CODIGO/funcoes.c
+++ Estrutura-Dados-Trabalho/CODIGO/funcoes.c
@@ -23,3 +23,38 @@ void BuscaLinear(int lista[], int tamanho, int busca)
         printf("número %d não encontrado\n", busca);
     }
 }
+
+void BuscaBinaria(int lista[], int tamanho, int busca){
+    int inicio, fim, comparacoes, encontrou;
+    encontrou = 0;
+    inicio = 0;
+    fim = tamanho - 1;
+    comparacoes = 0;
+
+    while (inicio <= fim)
+    {
+        int meio = (inicio + fim) / 2;
+        comparacoes += 1;
+
+        if (lista[meio] == busca)
+        {
+            encontrou += 1;
+            printf("Encontrado na posição: %d\n", meio);
+            printf("Comparações realizadas: %d\n", comparacoes);
+            break;
+        }
+        else if (lista[meio] < busca)
+        {
+            inicio = meio + 1;
+        }
+        else
+        {
+            fim = meio - 1;
+        }
+    }
+
+    if (encontrou == 0)
+    {
+        printf("número %d não encontrado\n", busca);
+    }
+}
